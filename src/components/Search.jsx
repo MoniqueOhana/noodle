7564979c69fba2327946ae129d439a56e63a204e
Monique <moniqueohana@yahoo.com.br>
2024-09-30 17:01:24 -0300

    commit 2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,12 +6,19 @@ const Search = () => {
     const [query, setQuery]  = useState("");
     const [results, setResults] = useState([]);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
 
     const API_Key = 
     "4f0062523fc30ce49d3270f3805441fb782e3d1e3de9b95f7b3e11f0cf91453e";
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (!query) {
+            return;
+        }
+        setLoading(true);
 
         const URL = 'https://serpapi.com/search.json'
 
@@ -31,8 +38,9 @@ const Search = () => {
     } catch (err) {
         console.error(err.message);
         setError("Ocorreu um erro inesperado");
-    }
-        
+    } finally {
+        setLoading(false);
+    }     
     };
     
 
@@ -46,6 +54,30 @@ const Search = () => {
                 <input type="text" name="query" placeholder="Digite sua busca" onChange={(e) => setQuery(e.target.value)} />
                 <button type="submit">Buscar</button>
             </form>
+            <div>
+                <ul>
+                    {error ? (<h4>{error}</h4>) :
+                    loading ? (
+                    <h4>Carregando...</h4>
+                    ) : (
+                    results.map((item, index) => {
+
+                        return (
+                        <li key={index}>
+                            <a 
+                            href={item.link} 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            {item.title}</a>
+                            <p>{item.snippet}</p>
+
+                        </li>
+                        );
+                    }))
+                    };
+                </ul>
+            </div>
         </div>
         );
     };
